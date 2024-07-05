6d61c81c8f74b86329636d5c59141857970fc670
Vikas Singh <89316827+vikasis4@users.noreply.github.com>
2024-07-05 15:03:04 +0530

    done

--- index.js
+++ index.js
@@ -9,19 +9,28 @@ var io = new Server({
 var httpServer = http.createServer();
 var PORT = process.env.PORT || 9000;
 io.attach(httpServer);
+
 httpServer.listen(PORT, function () { return console.log("HTTP Server started at PORT:".concat(PORT)); });
+
+
 io.on('connection', function (socket) {
-    socket.join('room');
+
+    socket.on('room', function (data) {
+        console.log('rom');
+        socket.join(data)
+        socket.broadcast.to(data).emit('room-joined', data);
+    });
     socket.on('offer', function (data) {
-        console.log('offer');
-        socket.broadcast.to('room').emit('offer', data);
+        console.log('offer ', ' RoomId :- ', data.roomId);
+        socket.broadcast.to(data.roomId).emit('offer', { offer: data.offer });
     });
     socket.on('answer', function (data) {
-        console.log('ans');
-        socket.broadcast.to('room').emit('answer', data);
+        console.log('Answer', ' RoomId :- ', data.roomId);
+        socket.broadcast.to(data.roomId).emit('answer', { answer: data.answer });
     });
     socket.on('ice-candidates', function (data) {
-        console.log('ice');
-        socket.broadcast.to('room').emit('ice-candidates', data);
+        console.log('ice ', data.roomId);
+        socket.broadcast.to(data.roomId).emit('ice-candidates', { data: data.data });
     });
+
 });
